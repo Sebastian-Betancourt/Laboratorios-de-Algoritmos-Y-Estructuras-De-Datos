dad4f6d936300dd8d4d94990d1e92f0f324cdf89
Sebastian-Betancourt <sebasj@outlook.com.ar>
2024-05-08 20:49:54 -0500

    Updated

--- Laboratorio-03/Ejercicio1.py
+++ Laboratorio-03/Ejercicio1.py
@@ -1,2 +1,17 @@
-#Ejercicio
-print ("Hola")
+#Ejercicio 1
+#Declaración de variables
+print ("Hola, este es un programa para calcular \n" "el área de la placa del circuito")
+A = float (input("Ingrese el valor de la base mayor en este caso el lado A: "))
+C = float (input("Ingrese el valor de la base mayor en este caso el lado C: "))
+B = float (input("Ingrese el valor de la altura en este caso el lado B: "))
+
+# Operación
+
+Area =((A+C)*B/2)
+
+# Salida de datos
+print (f"El área de la placa del circuito es de {round(Area,2)} cm^2 ")
+
+
+
+
